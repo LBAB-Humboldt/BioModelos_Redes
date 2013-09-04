3d07b61a93d1a9de6a6d6bed37894927e4739529
Daniel <dlopez@humboldt.org.co>
2013-09-04 11:19:53 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biomodis::Application.config.secret_key_base = '51063ba5d8f1ed314ffebb4c4d292579dea666626d4cab9e82793af6281180270c9d65a94a6a35dd08383d5e43237116fef9f9d8aefa1fa73ba512512df0a0bd'
