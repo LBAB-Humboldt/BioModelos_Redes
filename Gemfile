02d9eb5fefdd60847528998ed60dd0a22e8fc037
Daniel <daniel@daniel-VirtualBox.(none)>
2013-09-04 14:23:22 -0500

    Security modifications

--- Gemfile
+++ Gemfile
@@ -5,8 +5,14 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.0.0'
+  gem 'capybara', '2.1.0'
 end
 
 gem 'sass-rails', '4.0.0'
@@ -15,6 +21,7 @@ gem 'coffee-rails', '4.0.0'
 gem 'jquery-rails', '2.2.1'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
+gem 'leaflet-rails'
 
 
 gem 'therubyracer', :platform => :ruby
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Biomodis::Application.config.secret_key_base = '51063ba5d8f1ed314ffebb4c4d292579dea666626d4cab9e82793af6281180270c9d65a94a6a35dd08383d5e43237116fef9f9d8aefa1fa73ba512512df0a0bd'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Biomodis::Application.config.secret_key_base = secure_token
